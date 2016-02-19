5245a68fec3d6445517e58064d1ddd537c2fc7d9
nic <netzeln@gmail.com>
2016-02-19 08:48:44 -0800

    add form and home route

--- app/app.php
+++ app/app.php
@@ -1,13 +1,16 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/~~~~~~.php";
+    require_once __DIR__."/../src/WordFinder.php";
 
-    session_start();
 
     $app = new Silex\Application();
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path'=>__DIR__."/../views"
     ));
+
+    $app->get('/', function() use ($app){
+        return $app['twig']->render("index.html.twig");
+    });
 
 
 
